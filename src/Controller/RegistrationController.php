3d0c29c926c5eb17f66326e6d6e7d31ce51432ff
od od <o.daffe@yahoo.fr>
2020-08-04 19:56:14 +0200

    Reset MDP

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -7,12 +7,15 @@ use App\Form\ResetPassType;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use App\Security\UserAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -74,16 +77,79 @@ class RegistrationController extends AbstractController
         $manager->flush();
         return $this->redirectToRoute('home_index');
     }
+
     /**
      * Demande d'email 
      * @Route("/passe-oublie", name="registration_forgotten")
      */
-    public function forgottenPass(Request $request)
+    public function forgottenPass(Request $request, UserRepository $userRepo, TokenGeneratorInterface $tokenGenerator, EntityManagerInterface $manager, \Swift_Mailer $mailer)
     {
         $form = $this->createForm(ResetPassType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $email = $form->get('email')->getData();
+            $user = $userRepo->findOneBy(['email'=>$email]);
+            if(!$user){
+                $this->addFlash('danger', 'Cette adresse email inconnue');
+                return $this->redirectToRoute('registration_forgotten');
+            }
+            //generer token et on try le token en bdd 
+            try {
+                $token = $tokenGenerator->generateToken();
+                $user->setResetToken($token);
+                $manager->flush();  
+            } catch (\Exception $e) {
+                $this->addFlash('danger', $e->getMessage());
+                return $this->redirectToRoute('registration_forgotten');
+            }
+            //generer l'url de l'action qui affiche le form modificateur le mdp afin de l'envoyer dans l'email
+            $url = $this->generateUrl('registration_reset', ['token'=>$token], UrlGeneratorInterface::ABSOLUTE_URL);
+            //Envoyer message user
+            $message = (new \Swift_Message('Mot de passe oublié'))
+            ->setFrom('votre@adresse.fr')
+            ->setTo($user->getEmail())
+            ->setBody("Bonjour,<br><br>Une demande de réinitialisation de mot de passe a été effectuée pour le site Guinée Fruits&Legumes.com. 
+                 Veuillez cliquer sur le lien suivant : <a href=$url> Modifier email </a>", 
+                 'text/html' 
+            ) ;
+            // On envoie l'e-mail
+            $mailer->send($message);
+
+            // On crée le message flash de confirmation
+            $this->addFlash('message', 'E-mail de réinitialisation du mot de passe envoyé !');
+
+            // On redirige vers la page de login
+            return $this->redirectToRoute('app_login');
+
+
+        }
         return $this->render('registration/forgotten_pass.html.twig',[
             'form'=>$form->createView()
         ]);
     }
+    /**
+     * Reset MDP 
+     * @Route("/reset/pass/{token}", name="registration_reset")
+     */
+    public function resetPass(Request $request, UserRepository $userRepo, $token, EntityManagerInterface $manager,UserPasswordEncoderInterface $passwordEncoder)
+    {
+        if($request->isMethod('post')){
+            $user = $userRepo->findOneBy(['reset_token'=>$token]);
+            if(!$user){
+                $this->addFlash('danger', 'Token Inconnu');
+                return $this->redirectToRoute('app_login');
+            }
+            //vider le token
+            $user->setResetToken("");
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+            $manager->flush();
+            $this->addFlash('message', 'Mot de passe mis à jour');
 
+            // On redirige vers la page de connexion
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('registration/reset_pass.html.twig',[
+            'token'=>$token
+        ]);
+    }
 }
