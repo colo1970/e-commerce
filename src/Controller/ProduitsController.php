11ea378b639b4d2a6f7709a070ae1c78b5074252
od od <o.daffe@yahoo.fr>
2020-08-21 18:54:19 +0200

    Ajout pagination

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Produits;
 use App\Repository\ProduitsRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,12 +15,13 @@ class ProduitsController extends AbstractController
     /**
      * @Route("/", name="produits_produits")
      */
-    public function produits(ProduitsRepository $produitRepo, SessionInterface $session)
+    public function produits(ProduitsRepository $produitRepo, SessionInterface $session, PaginatorInterface $paginator, Request $request)
     {
-        $produits =$produitRepo->findBy(['disponibilite' => 1]);
-        if(!$produits){
+        $produitsRepo = $produitRepo->findBy(['disponibilite' => 1]);
+        if(!$produitsRepo){
            throw $this->createNotFoundException('Pas produit disponible');
         }
+        $produits = $paginator->paginate($produitsRepo, $request->query->getInt('page', 1), 2 );
         //S'il y a un produit dans mon panier j'enleve son bouton ajouter au panier
         $panier = $session->get('panier', []) ;
         return $this->render('produits/produits.html.twig', [
